f93068a6277d5e31ce51f1b57ceb1bad9ebfc84f
Dev Dixit <devdixitsocial@gmail.com>
2025-04-11 07:53:53 +0530

    Intermediate step 1 complete

--- Intermediate/main.js
+++ Intermediate/main.js
@@ -8,7 +8,6 @@ const checkVoter = () => {
     ans >= 18 ? console.log('You can vote') : console.log('You can not vote');
   }
 }
-
 checkVoter();
 
 const discountChecker = (productPrice, disPercent) => {
@@ -21,7 +20,27 @@ const discountChecker = (productPrice, disPercent) => {
     console.log(finalPrice);
   }
 }
-
 let productPrice = 3000
 let disPercent = 10
 discountChecker(productPrice, disPercent);
+
+let day = 2;
+
+switch(day){
+  case 1: console.log('Monday');
+  break
+  case 2: console.log('Tuesday');
+  break
+  case 3: console.log('Wednesday');
+  break
+  case 4: console.log('Thrusday');
+  break
+  case 5: console.log('Friday');
+  break
+  case 6: console.log('Saturday');
+  break
+  case 7: console.log('Sunday');
+  break
+  default: console.log('Invalid Input')
+  break
+}
