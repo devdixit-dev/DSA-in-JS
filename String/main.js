cd217f62fcc5368838f7bd4b3b6c3e6944c6d2d5
Dev Dixit <devdixitsocial@gmail.com>
2025-04-16 09:04:35 +0530

    reverse string and check palindrome

--- String/main.js
+++ String/main.js
@@ -20,14 +20,47 @@ for(let i = 0; i<string.length; i++) {
   console.log(string.charAt(i));
 }
 
-// question 2 - print in reverse order
-let s = "hello"
-let rev = ""
+// question 2 - print in reverse order - this method is not usefull for long string (not effective)
+// let s = "naman"
+// let rev = ""
 
-for(let i = s.length-1; i>=0; i--){
-  rev = rev + s.charAt(i)
+// for(let i = s.length-1; i>=0; i--){
+//   rev = rev + s.charAt(i)
+// }
+
+// console.log(rev)
+
+// // check string is palindrome or not ?
+// if (rev == s) console.log("palindrome")
+// else console.log("no palindrome")
+
+// effective method with the pointer method (check palindrome)
+const strArray = "naman"
+
+let i = 0;
+let j = strArray.length-1
+
+let isPalindrome = true
+
+while(i<j){
+  if(strArray.charAt(i) != strArray.charAt(j)){
+    isPalindrome = false
+    break
+  }
+  i++
+  j--
 }
 
-console.log(rev)
+// ternary operator
+isPalindrome 
+? 
+console.log("palindrome") 
+: 
+console.log("no palindrome")
 
-// 26:22 - explanation
+
+// convert this string "AbCDeF" to "aBcdEf"
+// if char is uppercase convert it to lowercase, if lowercase then convert it to uppercase
+
+// hello
+// h - 1, e - 1, l - 2, o - 1
