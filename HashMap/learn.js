a6adc5b8b612658056117b846f41223e65c1d9d8
devdixit-dev <devdixitsocial@gmail.com>
2026-05-05 21:43:24 +0530

    add frequency counting and duplicate detection in hashmap example

--- HashMap/learn.js
+++ HashMap/learn.js
@@ -10,3 +10,24 @@ console.log("mango" in map); // false
 
 delete map["banana"]; // delete "banana"
 console.log(map);
+
+// Hashmap - search, insert and delete
+// Time complexity - O(1)
+
+const nums = [1,2,2,3,3,3];
+const freq = {};
+const seen = {};
+
+for(let num of nums) {
+  freq[num] = (freq[num] || 0) + 1;
+  console.log("freq[num]", freq[num]);
+  // 1 1 2 1 2 3
+
+  if(num in seen) {
+    console.log(num + " is a duplicate!")
+  }
+
+  seen[num] = true;
+}
+
+console.log(freq);
